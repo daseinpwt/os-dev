40eb061fa565cfd69deb48718ac944362cf18ed1
Wentao Pan <daseinpwt@gmail.com>
2019-06-02 18:23:15 +0200

    Finish gdt definition

--- src/protected_mode/gdt.asm
+++ src/protected_mode/gdt.asm
@@ -28,3 +28,29 @@ gdt_code:              ; the code segment descriptor
     db 0x0               ; Base  (bits 24-31)
 
 gdt_data:              ; the data segment descriptor
+    ; same as code segment except for the type flags:
+    ; type flags:   code = 0, expand down = 0, writable = 1, accessed = 0
+    ;                      0                0             1             0   -> 0010b
+    dw 0xffff            ; Limit (bits 0-15)
+    dw 0x0               ; Base  (bits 0-15)
+    db 0x0               ; Base  (bits 16-23)
+    db 10010010b         ; 1st flags, type flags
+    db 11001111b         ; 2nd flags, Limit (bits 16-19)
+    db 0x0               ; Base  (bits 24-31)
+
+gdt_end:                 ; The reason for putting a label at the end of the
+                         ; GDT is so we can have the assembler calculate
+                         ; the size of the GDT for the GDT descriptor (below)
+
+; GDT descriptor
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1   ; Size of our GDT, always less one
+                                 ; of the true size
+    dd gdt_start
+
+; GDT segments descriptor offsets, which are what
+; segment registers must contain when in protected
+; mode. 
+; 0x0 -> NULL; 0x08 -> CODE; 0x10 -> DATA;
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
